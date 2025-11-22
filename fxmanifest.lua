d7cacf59dc919e3d118bc3286cb280483d37189b
Trap <148614470+TraIPZY@users.noreply.github.com>
2025-11-22 13:05:31 -0500

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'TRAPZY'
+description 'Interactive Questionnaire'
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
+
+client_script 'client.lua'
+server_script 'server.lua'
